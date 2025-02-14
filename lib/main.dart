a515cf2c4173d036c51c59f392c4c06b1f54ad0d
kanyaphon4115 <kanyaporn4115k@gmail.com>
2025-02-14 09:40:40 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/questions_screen.dart';
 import 'package:myapp/start_screen.dart';
 
 void main() {
-  runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 4, 16, 255),
-                  Color.fromARGB(255, 75, 248, 230)
-                ],
-              ),
-            ),
-            child: const StartScreen()),
-      ),
-    );
-  }
+  runApp(const MaterialApp(
+      home: StartScreen(), // เรียก StartScreen โดยตรง
+    )
+  );
 }
